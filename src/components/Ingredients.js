a28bab3d89ea2b835b1aa076fc00fabc85b14657
Frederic Siluvangi <fredsilu@gmail.com>
2024-09-14 14:02:53 +0100

    CHANGES to structure

---
+++ src/components/Ingredients.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+
+
+export default function Ingredients() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>INGREDIENTS</Text>
+    </View>
+  );
+}
---
+++ src/components/Plats.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+
+
+export default function Plats() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>PLATS</Text>
+    </View>
+  );
+}
